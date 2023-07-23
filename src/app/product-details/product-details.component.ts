f92ae9c35ab589c3c7004a836eab8a4ced8665bb
nhattruongfieldit <nhattruongfieldit@gmail.com>
2023-07-23 16:11:40 +0700

    commit code for product details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { Product, products } from "../products";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get("productId"));
+    this.product = products.find((p) => productIdFromRoute === p.id);
+  }
+}
