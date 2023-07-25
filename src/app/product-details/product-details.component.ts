0848c7785efa89aed93306ace44e5783d336af4c
nhattruongfieldit <nhattruongfieldit@gmail.com>
2023-07-25 22:43:24 +0700

    commit code to main branch

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Product, products } from "../products";
 import { ActivatedRoute } from "@angular/router";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -10,10 +11,14 @@ import { ActivatedRoute } from "@angular/router";
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cartService:CartService) {}
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get("productId"));
     this.product = products.find((p) => productIdFromRoute === p.id);
+  }
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    window.alert('Product has been add to cart!');
   }
 }
