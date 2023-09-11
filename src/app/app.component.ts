d3cc5653344bac9c5c68f878d832452e179eca56
nhattruongfieldit <nhattruongfieldit@gmail.com>
2023-09-11 21:06:02 +0700

    commit code for cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { environment } from "src/environments/environment.development";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  constructor() {
+    console.log(environment.production);
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
